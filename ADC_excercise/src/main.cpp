c3de7a47d233d4e2641473a3d4ebdc7183f9ccf1
AdrianZivkovic2007 <210186@htlw10.at>
2025-05-03 20:09:34 +0200

    Add files via upload

--- ADC_excercise/src/main.cpp
+++ ADC_excercise/src/main.cpp
@@ -6,28 +6,37 @@
 
 #include <Arduino.h>
 
-const int potPin = A0;
-const unsigned long interval = 500;
-unsigned long previousMillis = 0;
+#define ADC_CHANNEL 0
+
+volatile int poti_value = 0;
 
 void setup() {
-Serial.begin(115200);
-ADCSRA |= (1 << ADPS2) | (1 << ADPS1);
-ADMUX = (1 << MUX0);
+  Serial.begin(115200);
+
+  // Enable ADC, enable ADC interrupt, set prescaler to 128 (16MHz / 128 = 125kHz)
+  ADCSRA |= (1 << ADEN) | (1 << ADIE) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+
+  // Select ADC channel and set internal 5V reference (AVcc)
+  ADMUX = (ADMUX & 0b11110000) | ADC_CHANNEL;
+  ADMUX |= (1 << REFS0);
+
+  sei();                  // Enable global interrupts
+  ADCSRA |= (1 << ADSC);  // Start first ADC conversion
 }
 
 void loop() {
-unsigned long currentMillis = millis();
+  float voltage = (poti_value * 5.0) / 1023;
 
-if (currentMillis - previousMillis >= interval) {
-previousMillis = currentMillis;
-ADCSRA |= (1 << ADSC);
-while (ADCSRA & (1 << ADSC));
+  Serial.print("Potentiometer analog value = ");
+  Serial.print(voltage, 2);
+  Serial.println(" V");
 
-int adcValue = ADC;
-float voltage = adcValue * (5.0 / 1023.0);
-Serial.print("Potentiometer analog value = ");
-Serial.print(voltage, 2);
-Serial.println(" V");
+  delay(500); // Print value every 500ms
 }
+
+// This ISR is called when an ADC conversion is complete
+ISR(ADC_vect) {
+  poti_value = ADC;        // Read ADC result
+  ADCSRA |= (1 << ADSC);   // Start next ADC conversion
 }
+
